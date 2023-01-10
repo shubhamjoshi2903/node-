e95fdc902996dd206d298056c2ab687275d8bab0
jsshubham <shubham.joshi@thewitslab.com>
2023-01-10 18:35:23 +0530

    feat: learn middleware

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+
+const express = require("express");
+
+const app = express();
+
+
+
+app.use((req, res, next) => {
+  console.log("middleware");
+});
+
+const server = http.createServer(app);
+
 server.listen(3000);
