af1903207e067ce030d58dbdc0bc4c23b2d0b0a1
jsshubham <shubham.joshi@thewitslab.com>
2023-02-03 15:54:08 +0530

    feat: new cart

--- models/product.js
+++ models/product.js
@@ -1,42 +1,48 @@
 const fs = require("fs");
 const path = require("path");
 
-const getProductFromFile = (cb) => {
-  const p = path.join(
-    path.dirname(process.mainModule.filename),
-    "data",
-    "products.json"
-  );
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
 
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    cb(JSON.parse(fileContent));
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductFromFile();
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    this.id = Math.random().toString();
+    getProductsFromFile((products) => {
       products.push(this);
-
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log("err", err);
+        console.log(err);
       });
     });
   }
 
   static fetchAll(cb) {
-    getProductFromFile(cb);
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile((products) => {
+      const product = products.find((p) => p.id === id);
+      cb(product);
+    });
   }
 };
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,24 @@ const path = require("path");
 
 const express = require("express");
 
-const productController = require("../controllers/products");
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get("/", productController.getProduct);
+router.get("/", shopController.getIndex);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/products/:productId", shopController.getProduct);
+
+// router.get("/products/delete");
+
+router.get("/cart", shopController.getCart);
+
+router.post("/cart", shopController.postCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
